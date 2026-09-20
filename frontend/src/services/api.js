2c44e4c1853b155a7fcc1c9a00c2cfd4ced0197a
farisgp2-prog <faris.gp2@gmail.com>
2026-09-20 13:16:33 +0700

    Change API baseURL to use environment variable
    Updated baseURL to use environment variable for production.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  //baseURL: "http://127.0.0.1:8000/api",
+   baseURL: import.meta.env.VITE_API_BASE_URL, // VERCEL FIX: Use environment variable instead of hardcoded localhost URL for production deployment
   headers: {
     "Content-Type": "application/json",
   },
